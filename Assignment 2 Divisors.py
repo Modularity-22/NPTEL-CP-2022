53bd1edb4bf81a94ab58da1415faff0d3c5b866c
Mridul Gupta <83527720+Modularity-22@users.noreply.github.com>
2022-02-03 13:26:50 +0530

    Update Assignment 2 Divisors.py

--- Assignment 2 Divisors.py
+++ Assignment 2 Divisors.py
@@ -1,3 +1,12 @@
+# You are given integers N and M.
+
+# Consider a sequence a of length N consisting of positive integers such that a1+a2+....+an = M. Find the maximum possible value of the greatest common divisor of a1, a2,...,an.
+
+# Constraints
+# All values in input are integers.
+# Output
+# Print the maximum possible value of the greatest common divisor of a sequence a1, a2,...,an that satisfies the condition.
+
 n, m = map(int, input().split())
 ans = 0
 for i in range(1, m+1):
